e9702e9c84a2acbe35a26043ff09d4b1abd52f34
M_Ibrokhim <ibrokhimmuysinov@gmail.com>
2025-02-19 18:06:05 +0300

    Weather app finished by M_Ibrokhiim

--- index.js
+++ index.js
@@ -20,11 +20,6 @@ async function callingToApi(){
         const APIcalling=await  fetch (`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${apiKey}&units=metric`)
         const apiJSONdata= await APIcalling.json()
         
-        // if(!apiJSONdata.ok){
-        //     alert('Did not find')
-        //     const showAppBody=document.getElementById('app-body-id')
-        //     showAppBody.style.display="none"
-        // }
 
         const city=document.getElementById('app-body-div1-city-id')
         city.innerHTML= await apiJSONdata.name
